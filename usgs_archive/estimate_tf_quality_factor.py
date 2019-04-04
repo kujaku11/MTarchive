c6ebccc267013ccfb5f126134bfb691ddd11a926
JP <peacock.jared@gmail.com>
2019-04-03 19:28:47 -0700

    adjusting qf estimation

--- usgs_archive/estimate_tf_quality_factor.py
+++ usgs_archive/estimate_tf_quality_factor.py
@@ -41,32 +41,38 @@ class EMTFStats(object):
         >>> s_df = q.summarize_data_quality(q_df) 
     """
 
-    def __init__(self, *args, **kwargs):
-        self.tf_dir = None
-        self.stat_limits = {'std':{5: (0, 5), 
-                                   4: (5, 15),
-                                   3: (15, 40),
-                                   2: (40, 80),
-                                   1: (80, 200),
-                                   0: (200, 1E36)},
-                            'corr':{5: (.95, 1.0), 
-                                    4: (.85, .95),
-                                    3: (.7, .85),
-                                    2: (.5, .7),
-                                    1: (.2, .5),
-                                    0: (-1.0, .2)},
-                            'diff':{5: (0., 5.), 
-                                    4: (5., 15.),
-                                    3: (15., 50.),
-                                    2: (50., 200.),
-                                    1: (200., 1000.),
-                                    0: (1000., 1E36)},
+    def __init__(self, tf_dir=None, *args, **kwargs):
+        self.tf_dir = tf_dir
+        self.stat_limits = {'std':{5: (0, .5), 
+                                   4: (.5, 1.25),
+                                   3: (1.25, 2.5),
+                                   2: (2.5, 10.0),
+                                   1: (10.0, 25.0),
+                                   0: (25.0, 1E36)},
+                            'corr':{5: (.975, 1.0), 
+                                    4: (.9, .975),
+                                    3: (.75, .9),
+                                    2: (.5, .75),
+                                    1: (.25, .5),
+                                    0: (-1.0, .25)},
+                            'diff':{5: (0., .5), 
+                                    4: (.5, 1.0),
+                                    3: (1.0, 2.0),
+                                    2: (2.0, 5.0),
+                                    1: (5., 10.),
+                                    0: (10., 1E36)},
                             'fit':{5: (0, 5), 
                                    4: (5, 15),
                                    3: (15, 50),
                                    2: (50, 100),
                                    1: (100, 200),
-                                   0: (200, 1E36)}}
+                                   0: (200, 1E36)},
+                            'bad':{5: (0, 3), 
+                                   4: (3, 6),
+                                   3: (6, 10),
+                                   2: (10, 15),
+                                   1: (15, 25),
+                                   0: (25, 1E36)}}
                             
         self.z_dict = {(0, 0): 'xx', (0, 1): 'xy', (1, 0): 'yx', (1, 1): 'yy'}
         self.t_dict = {(0, 0): 'x', (0, 1): 'y'}
@@ -78,7 +84,101 @@ class EMTFStats(object):
                      ['{2}_{0}_{1}'.format(ii, kk, ll) 
                       for ii in ['x', 'y'] 
                       for kk in ['std', 'corr', 'diff', 'fit'] 
-                      for ll in ['tipper']]
+                      for ll in ['tipper']] + \
+                     ['bad_points_{2}_{0}{1}'.format(ii, jj, ll) 
+                      for ii in ['x', 'y']
+                      for jj in ['x', 'y']  
+                      for ll in ['res', 'phase']] + \
+                     ['bad_points_tipper_{0}'.format(ii) 
+                      for ii in ['x', 'y']]
+                     
+    def locate_bad_res_points(self, res):
+        """
+        try to locate bad points to remove
+        """
+        ### estimate levearge points, or outliers
+        ### estimate the median
+        med = np.median(res)
+        ### locate the point closest to the median
+        tol = np.abs(res-np.median(res)).min()
+        m_index = np.where((abs(res-med) >= tol*.95) &
+                           (abs(res-med) <= tol*1.05))[0][0]
+        r_index = m_index + 1
+        
+        bad_points = []
+        # go to the right
+        while r_index < res.shape[0]:
+            if abs(res[r_index]-res[r_index-1]) > np.cos(np.pi/4)*res[r_index]:
+                bad_points.append(r_index)
+            r_index += 1
+            
+        # go to the left
+        l_index = m_index - 1
+        while l_index > -1:
+            if abs(res[l_index]-res[l_index-1]) > np.cos(np.pi/4)*res[l_index]:
+                bad_points.append(l_index)
+            l_index -= 1
+            
+        return np.array(bad_points)
+    
+    def locate_bad_phase_points(self, phase, test=5):
+        """
+        try to locate bad points to remove
+        """
+        ### estimate levearge points, or outliers
+        ### estimate the median
+        med = np.median(phase)
+        ### locate the point closest to the median
+        ### locate the point closest to the median
+        tol = np.abs(phase-np.median(phase)).min()
+        m_index = np.where((abs(phase-med) >= tol*.95) & 
+                           (abs(phase-med) <= tol*1.05))[0][0]
+        r_index = m_index + 1
+        
+        bad_points = []
+        # go to the right
+        while r_index < phase.shape[0]:
+            if abs(phase[r_index]-phase[r_index-1]) > test:
+                bad_points.append(r_index)
+            r_index += 1
+            
+        # go to the left
+        l_index = m_index - 1
+        while l_index > -1:
+            if abs(phase[l_index]-phase[l_index-1]) > test:
+                bad_points.append(l_index)
+            l_index -= 1
+            
+        return np.array(bad_points)
+    
+    def locate_bad_tipper_points(self, tipper, test=5):
+        """
+        try to locate bad points to remove
+        """
+        ### estimate levearge points, or outliers
+        ### estimate the median
+        med = np.median(tipper)
+        ### locate the point closest to the median
+        tol = np.abs(tipper-np.median(tipper)).min()
+        m_index = np.where((abs(tipper-med) >= tol*.95) & 
+                           (abs(tipper-med) <= tol*1.05))[0][0]
+        r_index = m_index + 1
+        
+        bad_points = []
+        # go to the right
+        while r_index < tipper.shape[0]:
+            if abs(tipper[r_index]-tipper[r_index-1]) > test:
+                bad_points.append(r_index)
+            r_index += 1
+            
+        # go to the left
+        l_index = m_index - 1
+        while l_index > -1:
+            if abs(tipper[l_index]-tipper[l_index-1]) > test:
+                bad_points.append(l_index)
+            l_index -= 1
+            
+        return np.array(bad_points)
         
     def compute_statistics(self, tf_dir=None):
         """
@@ -103,6 +203,8 @@ class EMTFStats(object):
         
         if tf_dir is not None:
             self.tf_dir = tf_dir
+            
+        
 
         edi_list = glob.glob('{0}\*.edi'.format(self.tf_dir))
 
@@ -116,70 +218,109 @@ class EMTFStats(object):
             
             for ii in range(2):
                 for jj in range(2):
+                    flip = False
                     comp = self.z_dict[(ii, jj)]
-                    res = mt_obj.Z.resistivity[:, ii, jj]
+
+                    ### locate bad points
+                    bad_points_res = self.locate_bad_res_points(mt_obj.Z.resistivity[:, ii, jj])
+                    stat_array[kk]['bad_points_res_{0}'.format(comp)] = max([1, len(bad_points_res)])
+                    bad_points_phase = self.locate_bad_phase_points(mt_obj.Z.phase[:, ii, jj])
+                    stat_array[kk]['bad_points_phase_{0}'.format(comp)] = max([1, len(bad_points_res)])
                     ### need to get the data points that are within the reasonable range
                     ### and not 0
-                    nz_index = np.where((res > 10E-5) & 
-                                        (res < 10E9) & 
-                                        (res != 0.0))
+                    nz_index = np.nonzero(mt_obj.Z.resistivity[:, ii, jj])
+                    nz_index = np.delete(nz_index, bad_points_res)
+                    nz_index = np.delete(nz_index, bad_points_phase)
                     
-                    res = res[nz_index][::-1]
-                    res_err = mt_obj.Z.resistivity_err[nz_index, ii, jj][0][::-1]
-                    phase = mt_obj.Z.phase[nz_index, ii, jj][0][::-1]
-                    phase_err = mt_obj.Z.phase_err[nz_index, ii, jj][0][::-1]
-                    f = mt_obj.Z.freq[nz_index][::-1]
+                    f = mt_obj.Z.freq[nz_index]
+                    res = mt_obj.Z.resistivity[nz_index, ii, jj]
+                    res_err = mt_obj.Z.resistivity_err[nz_index, ii, jj]
+                    phase = mt_obj.Z.phase[nz_index, ii, jj]
+                    phase_err = mt_obj.Z.phase_err[nz_index, ii, jj]
+                    
+                    if len(f) < 2:
+                        print(mt_obj.station, comp, nz_index)
+                        continue
+                    
+                    # need to sort the array to be ordered with assending 
+                    # frequency.  Check to see if f is ascending, if not flip
+                    if f[0] > f[1]: 
+                        flip = True
+                        f = f[::-1]
+                        res = res[::-1]
+                        res_err = res_err[::-1]
+                        phase = phase[::-1]
+                        phase_err = phase_err[::-1]
+                        
                     ### make parameter for least squares fit
                     k = 7 # order of the fit
                     # knots, has to be at least to the bounds of f
                     t = np.r_[(f[0],)*(k+1), 
-                              [1],
+                              [min(1, f.mean())],
                               (f[-1],)*(k+1)]
             
                     ### estimate a least squares fit
-                    ls_res = interpolate.make_lsq_spline(f, res, t, k)
-                    ls_phase = interpolate.make_lsq_spline(f, phase, t, k)
+                    try:
+                        ls_res = interpolate.make_lsq_spline(f, res, t, k)
+                        ls_phase = interpolate.make_lsq_spline(f, phase, t, k)
                     
-                    ### compute a standard deviation between the ls fit and data 
-                    stat_array[kk]['res_{0}_fit'.format(comp)] = (res-ls_res(f)).std()
-                    stat_array[kk]['phase_{0}_fit'.format(comp)] = (phase-ls_phase(f)).std()
-                    
-                    stat_array[kk]['res_{0}_std'.format(comp)] = res_err.mean()
-                    stat_array[kk]['phase_{0}_std'.format(comp)] = phase_err.mean()
+                        ### compute a standard deviation between the ls fit and data 
+                        stat_array[kk]['res_{0}_fit'.format(comp)] = (res-ls_res(f)).std()
+                        stat_array[kk]['phase_{0}_fit'.format(comp)] = (phase-ls_phase(f)).std()
+                    except (ValueError, np.linalg.LinAlgError) as error:
+                        stat_array[kk]['res_{0}_fit'.format(comp)] = np.NaN
+                        stat_array[kk]['phase_{0}_fit'.format(comp)] = np.NaN
+                        print('{0} {1} {2}'.format(mt_obj.station, 
+                                                   comp, error))
+                    ### taking median of the error is more robust
+                    stat_array[kk]['res_{0}_std'.format(comp)] = np.median(res_err)
+                    stat_array[kk]['phase_{0}_std'.format(comp)] = np.median(phase_err)
                     
                     ### estimate smoothness
                     stat_array[kk]['res_{0}_corr'.format(comp)] = np.corrcoef(res[0:-1], res[1:])[0, 1]
                     stat_array[kk]['phase_{0}_corr'.format(comp)] = np.corrcoef(phase[0:-1], phase[1:])[0, 1]
                     
                     ### estimate smoothness with difference
-                    stat_array[kk]['res_{0}_diff'.format(comp)] = np.std(np.diff(res))/abs(np.mean(np.diff(res)))
-                    stat_array[kk]['phase_{0}_diff'.format(comp)] = np.std(np.diff(phase))/abs(np.mean(np.diff(phase)))
+                    stat_array[kk]['res_{0}_diff'.format(comp)] = np.abs(np.median(np.diff(res)))
+                    stat_array[kk]['phase_{0}_diff'.format(comp)] = np.abs(np.median(np.diff(phase)))
                     
                     ### compute tipper
                     if ii == 0:
                         tcomp = self.t_dict[(0, jj)]
                         t_index = np.nonzero(mt_obj.Tipper.amplitude[:, 0, jj])
-                        if t_index[0].size == 0:
+                        bad_points_t = self.locate_bad_tipper_points(mt_obj.Tipper.amplitude[:, 0, jj])
+                        stat_array[kk]['bad_points_tipper_{0}'.format(tcomp)] = max([1, len(bad_points_t)])
+                        t_index = np.delete(t_index, bad_points_t)
+                        if t_index.size == 0:
                             continue
                         else:
-                            tmag = mt_obj.Tipper.amplitude[t_index, 0, jj][0][::-1]
-                            tmag_err = mt_obj.Tipper.amplitude_err[t_index, 0, jj][0][::-1]
-                            tip_f = mt_obj.Tipper.freq[t_index][::-1]
+                            tmag = mt_obj.Tipper.amplitude[t_index, 0, jj]
+                            tmag_err = mt_obj.Tipper.amplitude_err[t_index, 0, jj]
+                            tip_f = mt_obj.Tipper.freq[t_index]
+                            if flip:
+                                tmag = tmag[::-1]
+                                tmag_err = tmag_err[::-1]
+                                tip_f = tip_f[::-1]
                             tip_t = np.r_[(tip_f[0],)*(k+1), 
-                                          [1],
+                                          [min(1, tip_f.mean())],
                                           (tip_f[-1],)*(k+1)]
-
-                            ls_tmag = interpolate.make_lsq_spline(tip_f, tmag,
-                                                                  tip_t, k)
-                            stat_array[kk]['tipper_{0}_fit'.format(tcomp)] = np.std(tmag-ls_tmag(tip_f))
+                            try:
+                                ls_tmag = interpolate.make_lsq_spline(tip_f, tmag,
+                                                                      tip_t, k)
+                            except (ValueError, np.linalg.LinAlgError) as error:    
+                                stat_array[kk]['tipper_{0}_fit'.format(tcomp)] = np.std(tmag-ls_tmag(tip_f))
+                                stat_array[kk]['tipper_{0}_fit'.format(tcomp)] = np.NaN
+                                print('{0} {1} {2}'.format(mt_obj.station, 
+                                                           tcomp, error))
                             stat_array[kk]['tipper_{0}_std'.format(tcomp)] = tmag_err.mean()
                             stat_array[kk]['tipper_{0}_corr'.format(tcomp)] = np.corrcoef(tmag[0:-1], tmag[1:])[0, 1]
                             stat_array[kk]['tipper_{0}_diff'.format(tcomp)] = np.std(np.diff(tmag))/abs(np.mean(np.diff(tmag)))
                             
         ### write file
         df = pd.DataFrame(stat_array, index=station_list)
+        df = df.replace(0, np.NAN)
         df.to_csv(os.path.join(self.tf_dir, 'tf_quality_statistics.csv'),
-                  index=True)
+                  index=True, na_rep='NaN')
         
         return df
     
@@ -217,8 +358,8 @@ class EMTFStats(object):
                                       for key in sorted(self.types)]),
                               index=stat_df.index)
         for col in qual_df.columns:
-            qual_df[col].values[:] = 0
-        
+            qual_df[col].values[:] = np.NaN
+
         ### loop over quality factors
         for qkey in self.stat_limits.keys():
             for column in qual_df.columns:
@@ -228,7 +369,8 @@ class EMTFStats(object):
                                         (stat_df[column] <= cvalues[1])] = ckey
             
         ### write out file
-        qual_df.to_csv(os.path.join(self.tf_dir, 'tf_quality_estimate.csv'))
+        qual_df.to_csv(os.path.join(self.tf_dir, 'tf_quality_estimate.csv'),
+                       na_rep='NaN')
         
         return qual_df
     
@@ -258,7 +400,8 @@ class EMTFStats(object):
             raise ValueError("No DataFrame to analyze")
         
         ### compute median value
-        summarized_df = quality_df.median(axis=1)
+        summarized_df = quality_df.median(axis=1).round()
+        
         summarized_df.to_csv(os.path.join(self.tf_dir, 'tf_quality.csv'),
                              header=False)
         return summarized_df
