369ec944ecbd4ccae55ff4e879676e3a1a518f44
JP <peacock.jared@gmail.com>
2021-06-10 14:36:31 -0700

    updating xml handling

--- usgs_archive/xml_utils.py
+++ usgs_archive/xml_utils.py
@@ -46,23 +46,30 @@ reproduce copyrighted items for other than personal use must be secured from
 the copyright owner.
 ------------------------------------------------------------------------------
 """
-
-# built in Python imports
+# =============================================================================
+# Imports
+# =============================================================================
 import os
 import collections
-import warnings
+from collections import OrderedDict
+import json
 from pathlib import Path
 import unicodedata
+from dateutil import parser
 
 from defusedxml import lxml
 from lxml import etree as etree
 
-try:
-    import pandas as pd
-except ImportError:
-    warnings.warn("Pandas library not installed, dataframes disabled")
-    pd = None
+import pandas as pd
 
+# =============================================================================
+# Global Variables
+# =============================================================================
+fgdc_dir = Path(__file__).parent.parent.joinpath("fgdc_standards")
+FGDC_XSD_NAME = fgdc_dir.joinpath("fgdc-std-001-1998-annotated.xsd")
+BDP_LOOKUP = fgdc_dir.joinpath("bdp_lookup.json")
+    
+# =============================================================================
 
 def xml_document_loader(xml_locator):
     """
@@ -424,7 +431,6 @@ class XMLRecord(object):
 
         1) File path/name on the local filesystem that exists and can be read
         2) String containing an XML Record.
-        3) URL containing an XML record
 
         Parameters
         ----------
@@ -443,25 +449,6 @@ class XMLRecord(object):
                 # they passde us a file path
                 self.record = lxml.parse(self.fname)
                 self._root = self.record.getroot()
-            else:
-                from pymdwizard.core import utils
-
-                try:
-                    if utils.url_validator(contents):
-                        contents = utils.requests_pem_get(contents).text
-                except:
-                    pass
-                self.fname = None
-
-                if contents[:3] == "ï»¿":
-                    # string contents start with the BOM strip this
-                    contents = contents[3:]
-                if type(contents) == str:
-                    # we need bytes not string
-                    contents = contents.encode("utf-8")
-
-                self._root = string_to_node(contents)
-                self.record = etree.ElementTree(self._root)
 
         except etree.XMLSyntaxError:
             self.fname = None
@@ -511,9 +498,8 @@ class XMLRecord(object):
             or
             pandas dataframe
         """
-        from pymdwizard.core import fgdc_utils
 
-        return fgdc_utils.validate_xml(
+        return validate_xml(
             self._contents.to_xml(), xsl_fname=schema, as_dataframe=as_dataframe
         )
 
@@ -586,7 +572,7 @@ class XMLNode(object):
             )
             result = result.rstrip()
         else:
-            result = "{}<{}>".format("  " * level, self.tag, self.tag)
+            result = "{}<{}>".format("  " * level, self.tag)
             for child in self.children:
                 if type(self.__dict__[child.tag]) == XMLNode:
                     child = self.__dict__[child.tag]
@@ -976,3 +962,228 @@ def split_tag(tag):
         fgdc_tag = tag
         index = 0
     return fgdc_tag, index
+
+
+
+
+def validate_xml(xml, xsl_fname="fgdc", as_dataframe=False):
+    """
+
+    Parameters
+    ----------
+    xml : lxml document
+                or
+          filename
+                or
+          string containing xml representation
+
+    xsl_fname : str (optional)
+                can be one of:
+                'fgdc' - uses the standard fgdc schema
+                        ../resources/FGDC/fgdc-std-001-1998-annotated.xsd
+                full file path to another local schema.
+
+                if not specified defaults to 'fgdc'
+    as_dataframe : bool
+                used to specify return format (list of tuples or dataframe)
+
+    Returns
+    -------
+        list of tuples
+        (xpath, error message, line number)
+        or
+        pandas dataframe
+    """
+
+    if xsl_fname.lower() == "fgdc":
+        xsl_fname = FGDC_XSD_NAME
+    else:
+        xsl_fname = xsl_fname
+
+    xmlschema = load_schema(xsl_fname.as_posix())
+    xml_doc = xml_document_loader(xml)
+    xml_str = node_to_string(xml_doc)
+
+    tree_node = string_to_node(xml_str.encode("utf-8"))
+    lxml._etree._ElementTree(tree_node)
+
+    errors = []
+    srcciteas = []
+
+    src_xpath = "dataqual/lineage/srcinfo/srccitea"
+    src_nodes = tree_node.xpath(src_xpath)
+    for i, src in enumerate(src_nodes):
+        srcciteas.append(src.text)
+        if src.text is None:
+            if len(src_nodes) == 1:
+                errors.append(
+                    (
+                        "metadata/" + src_xpath,
+                        "source citation abbreviation cannot be empty",
+                        1,
+                    )
+                )
+            else:
+                xpath = "metadata/dataqual/lineage/srcinfo[{}]/srccitea"
+                errors.append(
+                    (
+                        xpath.format(i + 1),
+                        "source citation abbreviation cannot be empty",
+                        1,
+                    )
+                )
+    procstep_xpath = "dataqual/lineage/procstep"
+    procstep_nodes = tree_node.xpath(procstep_xpath)
+    for proc_i, proc in enumerate(procstep_nodes):
+        srcprod_nodes = proc.xpath("srcprod")
+        for srcprod_i, srcprod in enumerate(srcprod_nodes):
+            srcciteas.append(srcprod.text)
+            if srcprod.text is None:
+                error_xpath = procstep_xpath
+                if len(procstep_nodes) > 1:
+                    error_xpath += "[{}]".format(proc_i + 1)
+                error_xpath += "/srcprod"
+                if len(srcprod_nodes) > 1:
+                    error_xpath += "[{}]".format(proc_i + 1)
+                errors.append(
+                    (
+                        "metadata/" + error_xpath,
+                        "source produced abbreviation cannot be empty",
+                        1,
+                    )
+                )
+
+    srcused_xpath = "dataqual/lineage/procstep/srcused"
+    srcused_nodes = tree_node.xpath(srcused_xpath)
+    for i, src in enumerate(srcused_nodes):
+        if src.text not in srcciteas:
+            if len(srcused_nodes) == 1:
+                errors.append(
+                    (
+                        "metadata/" + srcused_xpath,
+                        "Source Used Citation Abbreviation {} "
+                        "not found in Source inputs "
+                        "used".format(src.text),
+                        1,
+                    )
+                )
+            else:
+                xpath = "metadata/dataqual/lineage/procstep[{}]/srcused"
+                errors.append(
+                    (
+                        xpath.format(i + 1),
+                        "Source Used Citation Abbreviation {} "
+                        "not found in Source inputs "
+                        "used".format(src.text),
+                        1,
+                    )
+                )
+
+    if xmlschema.validate(tree_node) and not errors:
+        return []
+
+    line_lookup = dict(
+        [
+            (e.sourceline, tree_node.getroottree().getpath(e))
+            for e in tree_node.xpath(".//*")
+        ]
+    )
+    sourceline = tree_node.sourceline
+    line_lookup[sourceline] = tree_node.getroottree().getpath(tree_node)
+
+    fgdc_lookup = get_fgdc_lookup()
+
+    for error in xmlschema.error_log:
+        error_msg = clean_error_message(error.message, fgdc_lookup)
+        try:
+            errors.append((line_lookup[error.line][1:], error_msg, error.line))
+        except KeyError:
+            errors.append(("Unknown", error_msg, error.line))
+
+    errors = list(OrderedDict.fromkeys(errors))
+
+    if as_dataframe:
+        cols = ["xpath", "message", "line number"]
+        return pd.DataFrame.from_records(errors, columns=cols)
+    else:
+        return errors
+
+
+def get_fgdc_lookup():
+    """
+    Loads the local resource, 'bdp_lookup' into a json object
+
+    Returns
+    -------
+        json fgdc item lookup
+    """
+    annotation_lookup_fname = BDP_LOOKUP
+    try:
+        with open(annotation_lookup_fname, encoding="utf-8") as data_file:
+            annotation_lookup = json.loads(data_file.read())
+    except TypeError:
+        with open(annotation_lookup_fname) as data_file:
+            annotation_lookup = json.loads(data_file.read())
+
+    return annotation_lookup
+
+
+def clean_error_message(message, fgdc_lookup=None):
+    """
+    Returns a cleaned up, more informative translation
+    of a raw xml schema error message.
+    Empty or missing elements are described in plain English
+
+    Parameters
+    ----------
+    message : str
+              The raw message we will be cleaning up
+
+    Returns
+    -------
+        str : cleaned up error message
+    """
+    parts = message.split()
+    if "Missing child element" in message:
+        clean_message = "The {} is missing the expected element(s) '{}'"
+        clean_message.format(parts[1][:-1], parts[-2])
+    elif (
+        r"' is not accepted by the pattern '\s*\S(.|\n|\r)*'" in message
+        or "'' is not a valid value of the atomic type" in message
+    ):
+        shortname = parts[1][:-1].replace("'", "")
+        try:
+            longname = fgdc_lookup[shortname]["long_name"]
+        except (KeyError, TypeError):
+            longname = None
+
+        if longname is None:
+            name = shortname
+        else:
+            name = "{} ({})".format(longname, shortname)
+
+        clean_message = "The value for {} cannot be empty"
+        clean_message = clean_message.format(name)
+    else:
+        clean_message = message
+    return clean_message
+
+
+def format_date(date_input):
+    """
+    Convert a Python date object into an FGDC string format YYYYMMDD
+
+    Parameters
+    ----------
+    date_input : str or datetime
+            if str provided must be in format that dateutil's parser can handle
+    Returns
+    -------
+        str : date formated in FGDC YYYYMMDD format
+    """
+
+    if type(date_input) == str:
+        date_input = parser.parse(date_input)
+
+    return date_input.strftime("%Y%m%d")
+
